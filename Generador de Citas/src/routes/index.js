de618a48cbb32668dd86fc2d542256c6828376d0
José Hernández <joxshernandez@gmail.com>
2021-12-17 02:15:24 -0500

    metodos passport

--- Generador de Citas/src/routes/index.js
+++ Generador de Citas/src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const pool = require('../database');
 
 router.get('/' , (req, res)=>{
     res.render('index.html');
@@ -8,6 +8,26 @@ router.get('/' , (req, res)=>{
 
 router.get('/registro' , (req, res)=>{   
     res.render('registro.html');
+});
+
+router.post('/registro', async (req,res)=>{
+    const {nombre, apellido, cedula, correo, telefono, clave,nacimiento,sexo} = req.body;
+    //Creamos un objeto en el que se guardan los datos del registro
+    const newUsuario = {
+        nombre,
+        apellido,
+        cedula,
+        correo,
+        telefono,
+        clave,
+        nacimiento,
+        sexo
+    }
+    console.log(newUsuario);
+    //await espera a que termine de realizar esta funcion 
+    await pool.query('INSERT INTO usuario set ?', [newUsuario]);
+    res.send("Datos Recividos");
+
 });
 
 module.exports = router;
--- Generador de Citas/src/routes/links.js
+++ Generador de Citas/src/routes/links.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+//este pool hace referencia a la conexion en la base de datos
 const pool = require('../database');
 
-
+router.get('/add',(req,res)=>{
+    res.send('Prueba Exitosa');
+});
 
 module.exports = router;
