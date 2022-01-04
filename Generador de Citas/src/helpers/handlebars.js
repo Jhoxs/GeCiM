f0076f89f275e511a518e0964be751366646bb6a
José Hernández <joxshernandez@gmail.com>
2022-01-03 22:44:09 -0500

    Funciones de handlebars
    -Mostrar contenido dinamicamente dependiendo del rol

--- Generador de Citas/src/helpers/handlebars.js
+++ Generador de Citas/src/helpers/handlebars.js
@@ -1,2 +1,18 @@
+const Handlebars = require('handlebars');
 /*Esta seccion nos permite utilizar los templets hbs */
-
+//options será el contenido que se encuentra dentro de los condicionales
+Handlebars.registerHelper("esAdmin",(rol,options)=>{
+    if(rol === 'administrador'){
+        return options.fn(this);
+    }
+})
+Handlebars.registerHelper("esPaciente",(rol,options)=>{
+    if(rol === 'paciente' || rol ==='administrador'){
+        return options.fn(this);
+    }
+})
+Handlebars.registerHelper("esDoctor",(rol,options)=>{
+    if(rol === 'doctor'){
+        return options.fn(this);
+    }
+})
